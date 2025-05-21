3560d8bd2b6767e0548bb8fdc1cbcd8854ac9565
mks257 <138339735+mks257@users.noreply.github.com>
2025-05-20 22:51:45 -0500

    added all files

--- src/app.js
+++ src/app.js
@@ -4,5 +4,13 @@ const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
-
+app.get("/restaurants", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.findAll(); // Fetch all restaurants
+    res.json(restaurants); // Send as JSON
+  } catch (error) {
+    console.error("Failed to fetch restaurants:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 module.exports = app;
